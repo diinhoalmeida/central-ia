2a9e4357b9c7d164c4851cc6d57ccc217739e92a
Edilson Almeida <edifilho1409@outook.com>
2024-11-04 19:47:11 -0300

    fix: Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,9 +7,9 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
-      
+
       <Route path="/home" element={<Home />} />
-      
+
       <Route element={<ProtectedRoute redirectTo="/home" />}>
         <Route path="/chat" element={<ChatScreen />} />
       </Route>
