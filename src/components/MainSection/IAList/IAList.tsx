a3466da746701ad8a2c37b76f838b87365e61281
Edilson Almeida <edifilho1409@outook.com>
2024-11-04 09:22:38 -0300

    config: Applying Api Calls

--- src/components/MainSection/IAList/IAList.tsx
+++ src/components/MainSection/IAList/IAList.tsx
@@ -3,17 +3,17 @@ import { FaArrowRight } from "react-icons/fa";
 interface IAItem {
   name: string;
   company?: string;
+  image?: string; 
 }
 
 interface IAListProps {
   title: string;
   items: IAItem[];
-  isCompanyIncluded?: boolean;
 }
 
-export function IAList({ title, items, isCompanyIncluded = true }: IAListProps) {
+export function IAList({ title, items }: IAListProps) {
   return (
-    <div className="bg-white rounded-lg p-5 shadow-lg">
+    <div className="bg-white rounded-lg p-5 shadow-lg flex flex-col justify-between">
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       <ul className="space-y-4">
         {items.map((item, index) => (
@@ -22,16 +22,20 @@ export function IAList({ title, items, isCompanyIncluded = true }: IAListProps) 
             className="p-4 flex items-center justify-between hover:shadow-xl transition-shadow duration-200 cursor-pointer rounded-lg"
           >
             <div className="flex items-center gap-4">
-              <div className="h-12 w-12 bg-gray-100 rounded-lg flex items-center justify-center">
-                <span className="text-lg font-bold text-blue-600">
-                  {item.name[0]}
-                </span>
+              <div className="h-12 w-12 bg-gray-100 rounded-lg flex items-center justify-center overflow-hidden">
+                {item.image ? (
+                  <img src={item.image} alt={item.name} className="h-full w-full object-contain" />
+                ) : (
+                  <span className="text-lg font-bold text-blue-600">
+                    {item.name[0]}
+                  </span>
+                )}
               </div>
               <div>
                 <h3 className="text-xl font-semibold text-gray-800">
                   {item.name}
                 </h3>
-                {isCompanyIncluded && item.company && <p className="text-sm text-gray-500">{item.company}</p>}
+                {item.company && <p className="text-sm text-gray-500">{item.company}</p>}
               </div>
             </div>
           </li>
--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,40 +1,62 @@
+import { useEffect, useState } from "react";
 import { IAList } from "./IAList";
+import api from "../../config/axiosInstance";
+
+interface IAData {
+  name: string;
+  description?: string;
+  image?: string;
+}
+interface IATransformedItem {
+  name: string;
+  company?: string;
+  image?: string;
+}
 
 export function MainSection() {
-  const mostAccessedIAs = [
-    { name: "ChatGPT", company: "OpenAI" },
-    { name: "MidJourney", company: "MidJourney, Inc" },
-    { name: "Gemini", company: "Google" },
-    { name: "Copilot", company: "Microsoft" },
-    { name: "Jasper", company: "Jasper, Inc" },
-  ];
+  const [apiIAs, setApiIAs] = useState([]);
+  const [mostAccessedIAs, setMostAccessedIAs] = useState<IATransformedItem[]>(
+    []
+  );
 
-  const recentIAs = [
-    { name: "Copilot", company: "Microsoft" },
-    { name: "Jasper", company: "Jasper, Inc" },
-    { name: "ChatGPT", company: "OpenAI" },
-    { name: "MidJourney", company: "MidJourney, Inc" },
-    { name: "Gemini", company: "Google" },
-  ];
+  useEffect(() => {
+    api
+      .get("/ias")
+      .then((response) => {
+        setApiIAs(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar a lista de IAs:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (apiIAs.length > 0) {
+      setMostAccessedIAs(
+        apiIAs.map((ia: IAData) => ({
+          name: ia.name,
+          company: ia.description,
+          image: ia.image,
+        }))
+      );
+    }
+  }, [apiIAs]);
 
   const categories = [
     { name: "Produtividade" },
     { name: "Geração de Imagens" },
     { name: "Geração de Texto" },
     { name: "Análise de PDF" },
-    { name: "Análise de PDF" }
+    { name: "Análise de PDF" },
   ];
 
   return (
     <section className="bg-transparent relative z-10 top-[-170px] py-12 px-4">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md-1:grid-cols-3 gap-12">
         <IAList title="IAs Mais Acessadas" items={mostAccessedIAs} />
-        <IAList title="IAs Recentes" items={recentIAs} />
-        <IAList
-          title="Categorias Mais Acessadas"
-          items={categories}
-          isCompanyIncluded={false}
-        />
+        <IAList title="IAs Recentes" items={mostAccessedIAs} />
+        <IAList title="Categorias Mais Acessadas" items={categories} />
       </div>
     </section>
   );
